9e82889f65ad5f802e7c1408b021a670677ab719
firay24 <fisan.onodaconan@gmail.com>
2024-03-06 21:33:17 +0700

    navbar desktop

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,16 +1,34 @@
-import { HStack, Stack, Text } from "@chakra-ui/react";
+"use client";
+import {
+  Flex,
+  HStack,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
 import LogoImg from "@/assets/logo.png";
 import Link from "next/link";
 
 const Footer = () => {
+  const isMobile = useBreakpointValue({ base: true, md: false }) ?? false;
   return (
     <Stack bgColor="primary.yellow" marginTop={5} alignItems="center">
-      <Stack padding={5} alignItems="center" gap={4}>
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        alignItems="center"
+        justifyContent="space-between"
+        gap={4}
+        width={"full"}
+        paddingX={{ base: 5, md: 12 }}
+        paddingY={5}
+      >
         <Stack alignItems="center">
           <Image src={LogoImg} alt="logo" />
-          <Text fontSize="14px">Copyright © 2024 Templates</Text>
+          {isMobile ? (
+            <Text fontSize="14px">Copyright © 2024 Templates</Text>
+          ) : null}
         </Stack>
         <Stack textAlign="center">
           <Text fontWeight="semibold">Layanan Lain</Text>
@@ -28,7 +46,10 @@ const Footer = () => {
             </Text>
           </HStack>
         </Stack>
-      </Stack>
+        {!isMobile ? (
+          <Text fontSize="14px">Copyright © 2024 Templates</Text>
+        ) : null}
+      </Flex>
     </Stack>
   );
 };
