4704a67e9adbd33d4d61494e2cfe23654fff23e4
firay24 <fisan.onodaconan@gmail.com>
2024-03-08 00:39:19 +0700

    detail portofolio page desktop

--- app/portofolio/sections/hero.tsx
+++ app/portofolio/sections/hero.tsx
@@ -1,4 +1,12 @@
-import { Button, HStack, Heading, Stack, Text } from "@chakra-ui/react";
+"use client";
+import {
+  Flex,
+  HStack,
+  Heading,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
 import bgImg from "@/assets/bg.png";
@@ -6,38 +14,69 @@ import IdekeenImg from "@/assets/logo/idekeen.jpg";
 import { FaStar } from "react-icons/fa6";
 
 const HeroSections = () => {
+  const isMobile = useBreakpointValue({ base: true, md: false }) ?? false;
   return (
     <Stack position="relative" id="home">
-      <Stack
+      <Flex
         zIndex={50}
-        paddingX={5}
+        paddingX={{ base: 5, md: 14 }}
         gap={5}
-        alignItems="center"
-        justifyContent="center"
+        alignItems={{ base: "center", md: "end" }}
+        justifyContent={{ base: "center", md: "space-between" }}
         paddingY={10}
+        direction={{ base: "column", md: "row" }}
+        position={{ base: "relative", md: "absolute" }}
+        top={{ base: 0, md: "40%" }}
       >
-        <Stack width="60%">
-          <Image
-            src={IdekeenImg}
-            alt="logo image"
-            style={{ borderRadius: "10%" }}
-          />
-        </Stack>
-        <Stack textAlign="center" alignItems="center">
-          <HStack color="yellow.200" justifyContent="center">
+        <Flex
+          alignItems={{ base: "center", md: "end" }}
+          gap={5}
+          direction={{ base: "column", md: "row" }}
+        >
+          <Stack width={{ base: "60%", md: "15%" }}>
+            <Image
+              src={IdekeenImg}
+              alt="logo image"
+              style={{ borderRadius: "10%" }}
+            />
+          </Stack>
+          <Stack
+            textAlign={{ base: "center", md: "left" }}
+            alignItems={{ base: "center", md: "start" }}
+            paddingBottom={{ base: 0, md: 4 }}
+          >
+            {isMobile ? (
+              <HStack color="yellow.200" justifyContent="center">
+                {[...Array(5)].map((_, index: number) => (
+                  <FaStar key={index} />
+                ))}
+              </HStack>
+            ) : null}
+            <Stack>
+              <Heading as="h1" size="xl" color="primary.dark">
+                Idekeen: Landing Page
+              </Heading>
+              <Text fontSize="18px" fontWeight="semibold" color="yellow.700">
+                Basic Web
+              </Text>
+            </Stack>
+          </Stack>
+        </Flex>
+        {!isMobile ? (
+          <HStack
+            height={14}
+            color="primary.yellow"
+            justifyContent="center"
+            alignItems="start"
+            paddingBottom={20}
+          >
             {[...Array(5)].map((_, index: number) => (
               <FaStar key={index} />
             ))}
           </HStack>
-          <Heading as="h1" size="xl" color="primary.dark">
-            Idekeen: Landing Page
-          </Heading>
-          <Text fontSize="18px" fontWeight="semibold" color="yellow.700">
-            Basic Web
-          </Text>
-        </Stack>
-      </Stack>
-      <Stack overflow="hidden" zIndex={10}>
+        ) : null}
+      </Flex>
+      <Stack overflow="hidden" zIndex={10} height={{ base: "auto", md: 200 }}>
         <Image
           src={bgImg}
           alt="background"
--- app/portofolio/sections/tabs.tsx
+++ app/portofolio/sections/tabs.tsx
@@ -1,6 +1,8 @@
+"use client";
 import {
   AspectRatio,
   Button,
+  Flex,
   HStack,
   Heading,
   Stack,
@@ -10,6 +12,7 @@ import {
   TabPanels,
   Tabs,
   Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import React from "react";
 import { BsCheckCircleFill } from "react-icons/bs";
@@ -17,10 +20,11 @@ import GaleryPortofolio from "./GaleryPortofolio";
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
 
 const TabsSections = () => {
+  const isMobile = useBreakpointValue({ base: true, md: false }) ?? false;
   return (
-    <Stack minHeight="80vh">
+    <Stack minHeight="80vh" overflow="hidden" marginTop={{ base: 0, md: 24 }}>
       <Tabs paddingTop={10}>
-        <TabList border="none" paddingX={5}>
+        <TabList border="none" paddingX={{ base: 5, md: 14 }}>
           <Tab
             _selected={{ borderColor: "primary.dark", fontWeight: "semibold" }}
           >
@@ -35,19 +39,33 @@ const TabsSections = () => {
 
         <TabPanels>
           <TabPanel>
-            <Stack paddingX={5} paddingBottom={10}>
-              <Text>
-                Selain layanan web builder, layanan lain yaitu jasa design
-                flyer, logo, id card dan lain-lain yang berkaitan dengan digital
-                promote atau branding. Jadi, Qreative Tech berfokus pada layanan
-                digital kreatif dan tech service.
-              </Text>
-              <Text>
-                Qreative Tech dimulai pada tahun 2024, walaupun terbilang masih
-                dini, kami akan memberikan pelayanan semaksimal mungkin dengan
-                evaluasi berkala agar kami tetap terus berkembang.
-              </Text>
-              <Stack marginTop={4}>
+            <Flex
+              direction={{ base: "column", md: "row" }}
+              paddingX={{ base: 5, md: 14 }}
+              paddingBottom={10}
+              gap={{ base: 0, md: 10 }}
+            >
+              <Stack
+                fontSize={{ base: "16px", md: "18px" }}
+                width={{ base: "full", md: "70%" }}
+                gap={3}
+              >
+                <Text>
+                  Selain layanan web builder, layanan lain yaitu jasa design
+                  flyer, logo, id card dan lain-lain yang berkaitan dengan
+                  digital promote atau branding. Jadi, Qreative Tech berfokus
+                  pada layanan digital kreatif dan tech service.
+                </Text>
+                <Text>
+                  Qreative Tech dimulai pada tahun 2024, walaupun terbilang
+                  masih dini, kami akan memberikan pelayanan semaksimal mungkin
+                  dengan evaluasi berkala agar kami tetap terus berkembang.
+                </Text>
+              </Stack>
+              <Stack
+                marginTop={{ base: 4, md: 0 }}
+                fontSize={{ base: "16px", md: "18px" }}
+              >
                 <Text fontWeight={"semibold"}>Berikut fitur pada sistem:</Text>
                 <HStack>
                   <BsCheckCircleFill color="gray" />
@@ -62,32 +80,47 @@ const TabsSections = () => {
                   <Text>Design template basic</Text>
                 </HStack>
               </Stack>
-            </Stack>
+            </Flex>
             <Stack marginX={-5}>
               <GaleryPortofolio />
             </Stack>
           </TabPanel>
           <TabPanel>
-            <Stack width="full">
-              <AspectRatio ratio={16 / 9}>
-                <iframe
-                  src="https://www.youtube.com/embed/eKdkigGYFpE?si=IpFbhhQRIlPI0ah8"
-                  title="YouTube video player"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                />
-              </AspectRatio>
-              <Stack alignItems="center" gap={4} marginTop={6}>
-                <Heading as="h2" size="md">
-                  Pesan Website seperti ini sekarang
-                </Heading>
-                <Button
-                  rounded="full"
-                  colorScheme="red"
-                  rightIcon={<IoArrowForwardCircleOutline />}
+            <Stack
+              paddingX={{ base: 0, md: 14 }}
+              alignItems="center"
+              marginTop={{ base: 2, md: 4 }}
+            >
+              <Stack width={{ base: "full", md: "70%" }}>
+                <AspectRatio ratio={16 / 9}>
+                  <iframe
+                    src="https://www.youtube.com/embed/eKdkigGYFpE?si=IpFbhhQRIlPI0ah8"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  />
+                </AspectRatio>
+                <Stack
+                  alignItems="center"
+                  gap={{ base: 3, md: 6 }}
+                  marginTop={6}
                 >
-                  Pesan Sekarang
-                </Button>
-                <Button rounded="full">Konsultasi Dulu</Button>
+                  <Heading as="h2" size={{ base: "md", md: "xl" }}>
+                    Pesan Website seperti ini sekarang
+                  </Heading>
+                  <Flex gap={4} direction={{ base: "column", md: "row" }}>
+                    <Button
+                      rounded="full"
+                      colorScheme="red"
+                      rightIcon={<IoArrowForwardCircleOutline />}
+                      size={{ base: "md", md: "lg" }}
+                    >
+                      Pesan Sekarang
+                    </Button>
+                    <Button rounded="full" size={{ base: "md", md: "lg" }}>
+                      Konsultasi Dulu
+                    </Button>
+                  </Flex>
+                </Stack>
               </Stack>
             </Stack>
           </TabPanel>
